f6cc9e0fc3d0afeeb3144da617a2a8734eaaa621
Raima Varghese <raimavarghese.troodon@outlook.com>
2019-05-24 15:18:32 +0530

    styling

--- src/app/control-and-view/manager/manager-menu/manager-menu.page.ts
+++ src/app/control-and-view/manager/manager-menu/manager-menu.page.ts
@@ -16,33 +16,66 @@ export class ManagerMenuPage implements OnInit {
       icon: 'home'
     },
     {
-      title: ' Work Order',//first
+      title: ' WorkOrder ',//first
       url: '/manager-menu/work-order-dash-board',
       icon: "logo-buffer"
     },
+    {
+      title: 'Inspection Order',
+      url:  '/manager-menu/inspection-dash-board',
+      icon: "create"
+    },
+    
     {
       title: 'Scan For Work',//first
       url: '/manager-menu/scan-for-work',
       icon: "barcode"
     },
     {
-      title: 'Inspection Order',
-      url:  '/manager-menu/inspection-dash-board',
-      icon: "create"
+      title: 'Logout',
+      url:  '/manager-menu/login',
+      icon:"log-out"
     }
     
   ];
+  loginDetalis;
+  Username;
  
+
   constructor(private router: Router) {
+    
     this.router.events.subscribe((event: RouterEvent) => {
       if (event && event.url) {
         this.selectedPath = event.url;
       }
+      
     });
+  }
+  url_base64_decode(str) {
+    var output = str.replace('-', '+').replace('_', '/');
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += '==';
+        break;
+      case 3:
+        output += '=';
+        break;
+      default:
+        throw new Error('Illegal base64url string!');
+    }
+    return window.atob(output); //polifyll https://github.com/davidchambers/Base64.js
   }
  
   ngOnInit() {
- 
+    var token = localStorage.getItem('token');
+    localStorage['token'] = token;
+    var encodedProfile = token.split('.')[1];
+    var profile = JSON.parse(this.url_base64_decode(encodedProfile));
+  
+
+    this.Username=profile.username;
   }
 
 }

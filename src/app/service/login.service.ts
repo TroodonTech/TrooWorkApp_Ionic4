e778378e30c7594273de12533ea23a6ad056a253
rodneyThomas <rodney.troodon@outlook.com>
2019-05-28 18:08:19 +0530

    issue fix

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {ConnectionSettings  } from "./connectionSetting";
+import { ConnectionSettings } from "./connectionSetting";
 import { HTTP } from '@ionic-native/http/ngx';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(public http: HttpClient,private httppost: HTTP) { }
+  constructor(public http: HttpClient, private httppost: HTTP) { }
 
 
   authent(obj) {
-    const url = ConnectionSettings.AbsUrl+'/authenticate';
+    const url = ConnectionSettings.AbsUrl + '/authenticate';
     return this
       .http
-      .post (url, obj);
+      .post(url, obj);
+  }
+
+  setCurrentLocation(obj) {
+    const url = ConnectionSettings.Url + '/backgroundGeoLocation';
+    return this
+      .httppost
+      .post(url, obj, {});
   }
 }
